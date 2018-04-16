d698d1c326b390b998bd54e900d60591b522f4e4
Octowl <alsaff1987@gmail.com>
2018-04-16 14:35:20 +0300

    Toggle sidebar icon

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,28 +3,39 @@ import React from 'react';
 // Fontawesome
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faAngleLeft from '@fortawesome/fontawesome-free-solid/faAngleLeft';
+import faAngleRight from '@fortawesome/fontawesome-free-solid/faAngleRight';
 
 // Components
 import ChannelNavLink from './ChannelNavLink';
 
-function SideNav(props) {
-  const channelLinks = [{name: "all"}].map(
-    channel => <ChannelNavLink key={channel.name} channel={channel} />
-  )
-  return (
-    <div>
-      <ul className="navbar-nav navbar-sidenav" id="exampleAccordion">
-        {channelLinks}
-      </ul>
-      <ul className="navbar-nav sidenav-toggler">
-        <li className="nav-item">
-          <a className="nav-link text-center" id="sidenavToggler">
-            <FontAwesomeIcon icon={faAngleLeft} />
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+class SideNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {collapsed: false}
+  }
+
+  render () {
+    const channelLinks = [{name: "all"}].map(
+      channel => <ChannelNavLink key={channel.name} channel={channel} />
+    )
+    return (
+      <div>
+        <ul className="navbar-nav navbar-sidenav" id="exampleAccordion">
+          {channelLinks}
+        </ul>
+        <ul className="navbar-nav sidenav-toggler">
+          <li className="nav-item">
+            <a className="nav-link text-center" id="sidenavToggler"
+              onClick={() => this.setState(prevState => {
+                return {collapsed: !prevState.collapsed}
+              })}>
+              <FontAwesomeIcon icon={this.state.collapsed ? faAngleRight : faAngleLeft} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default SideNav;
